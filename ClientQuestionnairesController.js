31b9058dbb89a1b33f288a29fae6c6fbf37a493d
Lauren Fujioki <laurenfujioki@gmail.com>
2016-03-15 12:34:47 -0700

    Added files via upload

---
+++ ClientQuestionnairesController.js
@@ -0,0 +1,215 @@
+﻿(function () {
+    'use strict';
+
+    angular.module(APPNAME)
+        .controller('questionnairesController', QuestionnairesController);
+
+    QuestionnairesController.$inject = [
+          '$scope'
+        , '$baseController'
+        , '$observationService'
+        , '$uibModal'
+        , '$log'
+    ];
+
+    function QuestionnairesController(
+          $scope
+        , $baseController
+        , $observationService
+        , $uibModal
+        , $log
+        ) {
+
+        var vm = this;
+        $baseController.merge(vm, $baseController);
+        vm.$scope = $scope;
+        vm.$observationService = $observationService;
+        vm.notify = vm.$observationService.getNotifier($scope);
+
+        // Questionnaire properties
+        vm.questionnaireItem = null;
+        vm.questionnaireItems = null;
+        vm.selectedQuestionnaire = null;
+        vm.newQuestionnaire = null;
+        vm.isQuestionnaireForm = false;
+        vm.isQuestionnaireErrors = false;
+        vm.questionnaireRequest = {};
+        vm.isQuestionnairesData = false;
+
+        // CRUD
+        vm.addQuestionnaire = _addQuestionnaire;
+        vm.updateQuestionnaire = _updateQuestionnaire;
+        vm.deleteQuestionnaire = _deleteQuestionnaire;
+
+        // Ajax Success / Error
+        vm.onQuestionnairesGetSuccess = _onQuestionnairesGetSuccess;
+        vm.onQuestionnairesGetError = _onQuestionnairesGetError;
+        vm.onQuestionnaireAddSuccess = _onQuestionnaireAddSuccess;
+        vm.onQuestionnaireAddError = _onQuestionnaireAddError;
+        vm.onQuestionnaireUpdateSuccess = _onQuestionnaireUpdateSuccess;
+        vm.onQuestionnaireUpdateError = _onQuestionnaireUpdateError;
+        vm.onQuestionnaireDeleteSuccess = _onQuestionnaireDeleteSuccess;
+        vm.onQuestionnaireDeleteError = _onQuestionnaireDeleteError;
+
+        // Helpers
+        vm.onFireTabChangedEvent = _onFireTabChangedEvent;
+        vm.modalQuestionnaireOpen = _modalQuestionnaireOpen;
+
+
+        render();
+
+        function render() {
+            vm.setUpCurrentRequest(vm);
+            wireUpListeners();
+
+            vm.onFireTabChangedEvent(0);
+
+            vm.$observationService.questionnaires.getById(vm.onQuestionnairesGetSuccess, vm.onQuestionnairesGetError);
+        }
+
+        function wireUpListeners() {
+            vm.$systemEventService.listen('addObservationsDataRequest', onReceiveAddDataRequestEvent, vm);
+        }
+
+        function onReceiveAddDataRequestEvent(event, payload) {
+            vm.modalQuestionnaireOpen();
+        }
+
+        function _modalQuestionnaireOpen() {
+            var modalInstance = $uibModal.open({
+                templateUrl: '/Scripts/core/clientObservations/views/CONewQuestionnaireInput.html',
+                controller: 'questionnaireInputCtrl',
+                controllerAs: 'modal',
+                resolve: {
+                    item: function () {
+                        vm.newQuestionnaire = null;
+                        return vm.newQuestionnaire;
+                    }
+                }
+            });
+
+            modalInstance.result.then(function (selectedItem) {
+                vm.questionnaireRequest.name = selectedItem;
+                vm.addQuestionnaire(vm.questionnaireRequest);
+
+            }, function () {
+                vm.$alertService.warning('The Questionnaire form has not been saved.');
+            });
+        };
+
+        function _onQuestionnairesGetSuccess(data) {
+            vm.notify(function () {
+                if (data && data.items) {
+                    vm.questionnaireItems = data.items;
+                    vm.isQuestionnairesData = true;
+                }
+                else {
+                    vm.$alertService.info('There are no Questionnaires for this client.');
+                    vm.isQuestionnairesData = false;
+                }
+            });
+        }
+
+        function _onQuestionnairesGetError(jqXhr, error) {
+            vm.$alertService.error('There was a problem retrieving the Questionnaires.');
+        }
+
+        function _addQuestionnaire(eventData) {
+            if (eventData) {
+                vm.newQuestionnaire = eventData;
+                vm.$observationService.questionnaires.add(vm.newQuestionnaire, vm.onQuestionnaireAddSuccess, vm.isNewQuestionnaireErrors);
+            }
+            else {
+                vm.$alertService.error('Form submitted with invalid data');
+            }
+        }
+
+        function _onQuestionnaireAddSuccess(data) {
+            vm.notify(function () {
+                if (data && data.item) {
+                    vm.newQuestionnaire.id = data.item;
+
+                    if (!vm.questionnaireItems) {
+                        vm.questionnaireItems = [];
+                    }
+
+                    var today = new Date();
+                    var responseDate = moment.utc(today).toDate();
+                    vm.newQuestionnaire.dateModified = responseDate;
+
+                    vm.questionnaireItems.unshift(vm.newQuestionnaire);
+
+                    vm.$alertService.success('The Questionnaire "' + vm.newQuestionnaire.name + '" has been successfully added.');
+                    vm.newQuestionnaire = {};
+                    vm.isQuestionnairesData = true;
+                }
+            });
+        }
+
+        function _onQuestionnaireAddError(jqXhr, error) {
+            vm.$alertService.error('There was a problem adding the Questionnaire. Please try again.');
+        }
+
+        function _updateQuestionnaire(aQuestionnaire) {
+            vm.isQuestionnaireErrors = true;
+
+            if (aQuestionnaire) {
+                vm.$observationService.questionnaires.update(aQuestionnaire.id, aQuestionnaire.name,  aQuestionnaire, vm.onQuestionnaireUpdateSuccess, vm.onQuestionnaireUpdateError);
+            }
+            else {
+                vm.$alertService.error('Form submitted with invalid data');
+            }
+        }
+
+        function _onQuestionnaireUpdateSuccess(data, qName) {
+            vm.notify(function () {
+                if (data) {
+                    vm.$alertService.success('The Questionnaire "' + qName + '" has been successfully updated.');
+                }
+            });
+        }
+
+        function _onQuestionnaireUpdateError(jqXhr, error) {
+            vm.$alertService.error('There was a problem updating the Questionnaire. Please try again.');
+        }
+
+        function _deleteQuestionnaire(aQuestionnaire) {
+            vm.$observationService.questionnaires.disable(aQuestionnaire.id, aQuestionnaire.name, vm.onQuestionnaireDeleteSuccess, vm.onQuestionnaireDeleteError);
+        }
+
+        function _onQuestionnaireDeleteSuccess(data, qId, qName) {
+            vm.notify(function () {
+                if (data) {
+                    vm.$alertService.success('The Questionnaire "' + qName + '" has been successfully deleted.');
+
+                    var templist = vm.questionnaireItems;
+
+                    for (var i = 0; i < templist.length; i++) {
+                        if (templist[i].id == qId) {
+                            templist.splice(i, 1);
+                        }
+                    }
+
+                    if (typeof templist === 'undefined' || templist.length < 1) {
+                        vm.$alertService.info('There are no Questionnaires for this client.');
+                        vm.isQuestionnairesData = false;
+                    }
+                }
+            });
+        }
+
+        function _onQuestionnaireDeleteError(jqXhr, error) {
+            vm.$alertService.error('There was a problem deleting the Questionnaire. Please try again.');
+        }
+
+        function _onFireTabChangedEvent(idx, q) {
+            var broadcastData = {
+                tabIndex: idx
+                , title: 'Questionnaires'
+            }
+            vm.$systemEventService.broadcast('tabChanged', broadcastData);
+            vm.$systemEventService.broadcast("titleChanged", broadcastData);
+        }
+
+    }
+})();
---
+++ ClientQuestionnairesService.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data;
+using System.Data.SqlClient;
+
+using Client.Data;
+using Client.Web.Models.Requests.ClientObservations;
+using Client.Web.Domain.ClientObservation;
+
+namespace Client.Web.Services
+{
+    public class ClientObservationService : BaseService, IClientObservationsService
+    {
+        public int InsertQuestionnaire(COQuestionnaireAddRequest model, string userId, int accountId)
+        {
+            int id = 0;
+
+            DataProvider.ExecuteNonQuery(GetConnection, "dbo.COQuestionnaires_Insert"
+                   , inputParamMapper: delegate (SqlParameterCollection paramCollection)
+                   {
+                       paramCollection.AddWithValue("@Name", model.Name);
+
+                       paramCollection.AddWithValue("@UserId", userId);
+                       paramCollection.AddWithValue("@AccountId", accountId);
+
+                       SqlParameter p = new SqlParameter("@Id", System.Data.SqlDbType.Int);
+                       p.Direction = System.Data.ParameterDirection.Output;
+
+                       paramCollection.Add(p);
+
+                   }, returnParameters: delegate (SqlParameterCollection param)
+                   {
+                       int.TryParse(param["@Id"].Value.ToString(), out id);
+                   }
+                   );
+
+            return id;
+        }
+
+        public List<COQuestionnaire> GetQuestionnairesByAccount(int accountId)
+        {
+            List<COQuestionnaire> list = null;
+
+            DataProvider.ExecuteCmd(GetConnection, "dbo.COQuestionnaires_SelectByAccount"
+               , inputParamMapper: delegate (SqlParameterCollection paramCollection)
+               {
+                   paramCollection.AddWithValue("@AccountId", accountId);
+
+               }, map: delegate (IDataReader reader, short set)
+               {
+                   COQuestionnaire questionnaire = MapQuestionnaire(reader);
+
+                   if (list == null)
+                   {
+                       list = new List<COQuestionnaire>();
+                   }
+                   list.Add(questionnaire);
+               }
+               );
+
+            return list;
+        }
+
+        public void UpdateQuestionnaire(COQuestionnaireUpdateRequest model, int accountId)
+        {
+            DataProvider.ExecuteNonQuery(GetConnection, "dbo.COQuestionnaires_Update"
+                   , inputParamMapper: delegate (SqlParameterCollection paramCollection)
+                   {
+                       paramCollection.AddWithValue("@Name", model.Name);
+                       paramCollection.AddWithValue("@Id", model.Id);
+
+                       paramCollection.AddWithValue("@AccountId", accountId);
+
+                   }, returnParameters: delegate (SqlParameterCollection param)
+                   {
+                   }
+                   );
+        }
+
+        public void DisableQuestionnaire(int id, int accountId)
+        {
+            DataProvider.ExecuteNonQuery(GetConnection, "dbo.COQuestionnaires_Disable"
+                   , inputParamMapper: delegate (SqlParameterCollection paramCollection)
+                   {
+                       paramCollection.AddWithValue("@Id", id);
+                       paramCollection.AddWithValue("@AccountId", accountId);
+
+                   }, returnParameters: delegate (SqlParameterCollection param)
+                   {
+                   }
+                   );
+        }
+
+
+        //---------------------------------------
+
+        private COQuestionnaire MapQuestionnaire(IDataReader reader)
+        {
+            COQuestionnaire cq = new COQuestionnaire();
+            int startingIndex = 0;
+
+            cq.Id = reader.GetSafeInt32(startingIndex++);
+            cq.Name = reader.GetSafeString(startingIndex++);
+            cq.DateAdded = reader.GetSafeUtcDateTime(startingIndex++);
+            cq.DateModified = reader.GetSafeUtcDateTime(startingIndex++);
+
+            return cq;
+        }
+    }
+}
